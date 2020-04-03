cabf2bb32e517a1a9f1ad68f1ac4bc76da990500
Samuel Dantas <sam@samuelresolve.com>
2020-04-03 17:53:51 -0300

    Add Pagination & Login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const SessionController = require("./controllers/SessionController");
 const routes = express.Router();
+
+routes.post("/session", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
-routes.post("/incidents", IncidentController.create)
-routes.get("/incidents", IncidentController.index)
-routes.delete("/incidents/:id", IncidentController.delete)
+routes.post("/incidents", IncidentController.create);
+routes.get("/incidents", IncidentController.index);
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
