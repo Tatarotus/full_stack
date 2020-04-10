1ca8aec24f06c5021310359cf5cd21c330ef485a
Samuel Dantas <sam@samuelresolve.com>
2020-04-10 16:51:31 -0300

    Add profile route (list all incidents of an ong ID)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
 const SessionController = require("./controllers/SessionController");
 const routes = express.Router();
 
@@ -8,6 +9,8 @@ routes.post("/session", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
 
 routes.post("/incidents", IncidentController.create);
 routes.get("/incidents", IncidentController.index);
