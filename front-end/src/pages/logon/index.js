63046018de262bf54efe797cbd715c18c53d91bb
Samuel Dantas <sam@samuelresolve.com>
2020-04-10 01:29:08 -0300

    Add axios and localStorage login

--- front-end/src/pages/logon/index.js
+++ front-end/src/pages/logon/index.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import api from "../../services/api";
+
 import "./styles.css";
 
 import logo from "../../assets/Logo.svg";
 import hero from "../../assets/hero.svg";
 
-export default () => (
-  <div className="logon-container">
-    <section className="left">
-      <img src={logo} draggable={false} alt="logo" />
-      <div className="logon-section">
-        <form>
-          <label className="logon" htmlFor="ong_id">
-            Entre com sua ID:
-          </label>
-          <input type="text" id="ong_id" placeholder="Coloque aqui sua ID" />
-          <button className="button">Enter</button>
-        </form>
-      </div>
-    </section>
-    <section className="right">
-      <img src={hero} draggable={false} alt="hero-banner" />
-    </section>
-  </div>
-);
+export default () => {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("session", { id });
+
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
+    } catch {
+      alert("Falha no login, tente novament!");
+    }
+  }
+  return (
+    <div className="logon-container">
+      <section className="left">
+        <img src={logo} draggable={false} alt="logo" />
+        <div className="logon-section">
+          <form onSubmit={handleLogin}>
+            <label className="logon" htmlFor="ong_id">
+              Entre com sua ID:
+            </label>
+            <input
+              type="text"
+              id="ong_id"
+              value={id}
+              onChange={e => setId(e.target.value)}
+              placeholder="Coloque aqui sua ID"
+            />
+            <button className="button">Enter</button>
+          </form>
+        </div>
+      </section>
+      <section className="right">
+        <img src={hero} draggable={false} alt="hero-banner" />
+      </section>
+    </div>
+  );
+};
