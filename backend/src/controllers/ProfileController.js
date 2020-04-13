cb77184b5f3c15aeceb3bb89bda7f2c16f6db5fb
Samuel Dantas <sam@samuelresolve.com>
2020-04-12 22:42:54 -0300

    profile serve 6 items per call

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,35 +1,15 @@
 const connection = require("../database/connection");
-
-/*
-- Console the ong Id
-= check ong id in the db
--Return all incidents for the related ong 
-*/
-
-/*
-      .join("ongs", "ongs.id", "=", "incidents.ong_id") // Req Ong credenctials
-      .limit(5)
-      .offset((page - 1) * 5)
-      .select([
-        "incidents.*",
-        "ongs.name",
-        "ongs.email",
-        "ongs.whatsapp",
-        "ongs.city",
-        "ongs.uf"
-        */
 module.exports = {
   async index(req, res) {
     const { page } = req.query;
     const [count] = await connection("incidents").count();
     const ong_id = req.headers.authorization;
-    console.log(ong_id);
 
     const incidents = await connection("incidents")
       .where("ong_id", ong_id)
       .join("ongs", "ongs.id", "=", "incidents.ong_id")
-      .limit(5)
-      .offset((page - 1) * 5)
+      .limit(6)
+      .offset((page - 1) * 6)
       .select([
         "incidents.*",
         "ongs.name",
@@ -40,8 +20,11 @@ module.exports = {
       ]);
 
     res.header("X-Total-Count", count["count(*)"]);
+    let totalCount = count["count(*)"];
+    console.log(incidents);
     return res.json({
-      incidents
+      incidents,
+      count: totalCount
     });
   }
 };
